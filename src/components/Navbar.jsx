9bf37df3f5fd559318f1eec377e98fb9c34f9e5a
Linda Andersson <linda.29.andersson@gmail.com>
2022-10-31 11:44:05 +0100

    Added Links to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="search">Searcg</Link></li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
