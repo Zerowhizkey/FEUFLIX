20a36b994baf0ca4089389526ad52d5b7a701129
Linda Andersson <linda.29.andersson@gmail.com>
2022-10-31 11:54:16 +0100

    Corrected typo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="search">Searcg</Link></li>
+        <li><Link to="search">Search</Link></li>
       </ul>
     </div>
   );
