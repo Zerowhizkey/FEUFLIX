3c0f945cc5160a485be1754b572e8389ac7abfd1
Linda Andersson <linda.29.andersson@gmail.com>
2022-11-01 09:40:02 +0100

    Setup of html structure

--- src/components/modals/ModalCategories.jsx
+++ src/components/modals/ModalCategories.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const ModalCategories = () => {
   return (
-    <div>ModalCategories</div>
-  )
-}
+    <div className="background">
+      <section className="modal">
+        <button className="close-button">
+          <AiOutlineCloseCircle />
+        </button>
+        <div>
+          <h3>All categories</h3>
+          <ul>
+            <li>
+              <h4>Heading 4</h4>
+            </li>
+            <li>
+              <h4>Heading 4</h4>
+            </li>
+            <li>
+              <h4>Heading 4</h4>
+            </li>
+            <li>
+              <h4>Heading 4</h4>
+            </li>
+            <li>
+              <h4>Heading 4</h4>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default ModalCategories
+export default ModalCategories;
