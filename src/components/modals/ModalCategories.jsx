0a8f8e9790a47297e47b8734aac4a663a7efdd24
Linda Andersson <linda.29.andersson@gmail.com>
2022-11-01 11:17:43 +0100

    Setup of modal for categories with styling

--- src/components/modals/ModalCategories.jsx
+++ src/components/modals/ModalCategories.jsx
@@ -2,35 +2,55 @@ import React from "react";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import "./modalCategories.scss";
 
-const ModalCategories = () => {
+/* Instruction for setting up a modal
+
+* Import useState from react in the parent component
+
+* In the parent component: 
+const [isOpen, setIsOpen] = useState(false);
+
+* In render/button on parent component: 
+ <button onClick={() => setIsOpen(true)}>Modal</button>
+
+* Under the button in the parent component add on its own witout wraping in a element:
+{isOpen && <ModalCategories setIsOpen={setIsOpen} />}
+
+* Dont forget to import the child component (ModalCategories) in to the parent component 
+
+*/
+
+const ModalCategories = ({ setIsOpen }) => {
   return (
-    <div className="background">
-      <section className="modal">
-        <div className="modal-body">
-          <h3>All categories</h3>
-          <ul>
-            <li>
-              <h4>Heading 4</h4>
-            </li>
-            <li>
-              <h4>Heading 4</h4>
-            </li>
-            <li>
-              <h4>Heading 4</h4>
-            </li>
-            <li>
-              <h4>Heading 4</h4>
-            </li>
-            <li>
-              <h4>Heading 4</h4>
-            </li>
-          </ul>
-        </div>
-        <button className="close-button">
-          <AiOutlineCloseCircle />
-        </button>
-      </section>
-    </div>
+    <>
+      <div className="darkBG" onClick={() => setIsOpen(false)} />
+      <div className="modal-background">
+        <section className="modal">
+          <div className="modal-body">
+            <h3>All categories</h3>
+            <ul className="modal-list">
+              <li>
+                <h4>Heading 4</h4>
+              </li>
+              <li>
+                <h4>Heading 4</h4>
+              </li>
+              <li>
+                <h4>Heading 4</h4>
+              </li>
+              <li>
+                <h4>Heading 4</h4>
+              </li>
+              <li>
+                <h4>Heading 4</h4>
+              </li>
+            </ul>
+          </div>
+          <button className="close-button" onClick={() => setIsOpen(false)}>
+            <AiOutlineCloseCircle />
+          </button>
+        </section>
+      </div>
+    </>
   );
 };
 
