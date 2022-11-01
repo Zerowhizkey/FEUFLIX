5e167f5b55986c15e1e455a0a911707a0dfb00f6
Sandy <sandy.congreve@cmeducations.se>
2022-11-01 13:16:06 +0100

    Added functionality to searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,30 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Root from "./pages/Root";
 import Home from "./pages/Home";
-import Browse from "./pages/Browse";
+import Browse from "./pages/browse/Browse";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Root />,
-		children: [
-			{
-				path: "home",
-				element: <Home />,
-			},
-			{
-				path: "browse",
-				element: <Browse />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "browse",
+        element: <Browse />,
+      },
+    ],
+  },
 ]);
 function App() {
-	return (
-		<div className="App">
-			<RouterProvider router={router} />
-		</div>
-	);
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
