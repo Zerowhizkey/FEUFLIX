e9f7f6e64e1c50385ebb86ec71e61c8b8c52246a
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2022-11-01 10:31:13 +0100

    added grid for movie posters

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { Link } from "react-router-dom";
 import "./home.scss";
 
 const Home = () => {
+  const url =
+    "https://m.media-amazon.com/images/M/MV5BNDIzNDU0YzEtYzE5Ni00ZjlkLTk5ZjgtNjM3NWE4YzA3Nzk3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg";
   return (
     <div className="home">
       <header>
         <h1>Heading 1</h1>
         <button>
-          <AiOutlineSearch />
+          <Link to="/browse">
+            <AiOutlineSearch />
+          </Link>
         </button>
       </header>
       <section>
-        {/* grid here! map! */}
+        {/* map! */}
+        <img src={url} alt="figth club" />
+        <img src={url} alt="figth club" />
+        <img src={url} alt="figth club" />
+        <img src={url} alt="figth club" />
+        <img src={url} alt="figth club" />
+        <img src={url} alt="figth club" />
+        <img src={url} alt="figth club" />
+        <img src={url} alt="figth club" />
+        <img src={url} alt="figth club" />
+        <img src={url} alt="figth club" />
       </section>
     </div>
   );
