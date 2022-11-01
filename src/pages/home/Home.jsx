ea3cf28896d20f8d9023bb24836ad47933c224f4
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2022-11-01 11:28:23 +0100

    wireframe ish done, need api now

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import "./home.scss";
 
 const Home = () => {
+  const [show, setShow] = useState(false);
   const url =
     "https://m.media-amazon.com/images/M/MV5BNDIzNDU0YzEtYzE5Ni00ZjlkLTk5ZjgtNjM3NWE4YzA3Nzk3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg";
+
   return (
     <div className="home">
       <header>
@@ -16,19 +18,48 @@ const Home = () => {
           </Link>
         </button>
       </header>
-      <section>
-        {/* map! */}
-        <img src={url} alt="figth club" />
-        <img src={url} alt="figth club" />
-        <img src={url} alt="figth club" />
-        <img src={url} alt="figth club" />
-        <img src={url} alt="figth club" />
-        <img src={url} alt="figth club" />
-        <img src={url} alt="figth club" />
-        <img src={url} alt="figth club" />
-        <img src={url} alt="figth club" />
-        <img src={url} alt="figth club" />
-      </section>
+      {/* Popular:? */}
+      {/* Latest:? */}
+      <main>
+        {/* Upcoming: */}
+        <h3>Upcoming:</h3>
+        <section>
+          {/* map out posters onClick=modal*/}
+          <img
+            src={url}
+            alt="figth club"
+            onClick={() => [setShow(true), console.log(show)]}
+          />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+        </section>
+        {/* Top rated: */}
+        <h3>Top rated:</h3>
+        <section>
+          {/* map out posters onClick=modal*/}
+          <img
+            src={url}
+            alt="figth club"
+            onClick={() => [setShow(true), console.log(show)]}
+          />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+          <img src={url} alt="figth club" />
+        </section>
+      </main>
     </div>
   );
 };
