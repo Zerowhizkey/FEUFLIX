60cb385131079941d1e5d6d97c5152bcc54c818b
Sandy <sandy.congreve@cmeducations.se>
2022-11-02 15:11:25 +0100

    Modified grid and added properites to movie card

--- src/pages/browse/Browse.jsx
+++ src/pages/browse/Browse.jsx
@@ -5,47 +5,78 @@ import { useMovies } from "../../context/MovieContext";
 import "./browse.scss";
 
 const Browse = () => {
-	const [searchInput, setSearchInput] = useState("");
-	const { foundMovie, searchMovie } = useMovies();
+  const [searchInput, setSearchInput] = useState("");
+  const { foundMovie, searchMovie, popular, upcoming } = useMovies();
 
-	const handleSearch = () => {
-		searchMovie(searchInput);
-	};
+  const upcomingMovies = upcoming.results;
+  const popularMovies = popular.results;
 
-	return (
-		<div>
-			<div className="search-bar">
-				<AiOutlineSearch />
+  const handleSearch = () => {
+    searchMovie(searchInput);
+  };
+  console.log(foundMovie);
 
-				<input
-					className="search-input"
-					type="text"
-					placeholder="Search movie.."
-					onChange={(e) => setSearchInput(e.target.value)}
-				/>
-				<button className="search-button" onClick={handleSearch}>
-					<BsFillArrowRightCircleFill />
-				</button>
-			</div>
-			<section className="category-list">
-				<h3>
-					All Categories <AiOutlineDown />
-				</h3>
-			</section>
-			<h3>Results for : {searchInput}</h3>
-			<div>
-				{foundMovie.length !== 0 ? (
-					<div>
-						{foundMovie.results.map((movie) => (
-							<div key={movie.id}>{movie.title}</div>
-						))}
-					</div>
-				) : (
-					<>U suck</>
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="search-bar">
+        <AiOutlineSearch />
+
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search movie.."
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button className="search-button" onClick={handleSearch}>
+          <BsFillArrowRightCircleFill />
+        </button>
+      </div>
+      <section className="category-list">
+        <button>
+          <h5>All Categories</h5>
+          <AiOutlineDown />
+        </button>
+      </section>
+      <h4>Results for : {searchInput}</h4>
+
+      {foundMovie.length !== 0 ? (
+        <section>
+          {foundMovie.results.map((movie) => (
+            <div className="movie-card" key={movie.id}>
+              <div className="image-box">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                ></img>
+              </div>
+
+              <div className="title-card">
+                <p>{movie.release_date}</p>
+                <h4>{movie.title}</h4>
+              </div>
+            </div>
+          ))}
+        </section>
+      ) : (
+        <section>
+          {popularMovies &&
+            popularMovies.map((movie) => (
+              <div className="movie-card" key={movie.id}>
+                <div className="image-box">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  ></img>
+                </div>
+
+                <div className="title-card">
+                  <p>{movie.release_date}</p>
+                  <h4>{movie.title}</h4>
+                </div>
+              </div>
+            ))}
+        </section>
+      )}
+    </div>
+  );
 };
 
 export default Browse;
