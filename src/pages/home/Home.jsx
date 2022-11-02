62c9cead0990f1a42c779e67d78e33cc19e6535c
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2022-11-02 09:44:26 +0100

    modal setup on home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,20 +3,23 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { useMovies } from "../../context/MovieContext";
 import "./home.scss";
+import ModalMovie from "../../components/modals/ModalMovie";
 
 const Home = () => {
   const [show, setShow] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [id, setId] = useState("");
+
   const { movies } = useMovies();
 
   const movieList = movies.results;
   const url =
     "https://m.media-amazon.com/images/M/MV5BNDIzNDU0YzEtYzE5Ni00ZjlkLTk5ZjgtNjM3NWE4YzA3Nzk3XkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg";
 
-  console.log(movieList);
   return (
     <div className="home">
       <header>
-        <h1>Heading 1</h1>
+        <h1>FEUFLIX</h1>
         <button>
           <Link to="/browse">
             <AiOutlineSearch />
@@ -29,48 +32,18 @@ const Home = () => {
         {/* Upcoming: */}
         <h3>Upcoming:</h3>
         <section>
-          {/* map out posters onClick=modal*/}
           {movieList &&
             movieList.map((movie) => (
-              <div key={movie.id}>
-                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}></img>
+              <div key={movie.id} onClick={() => [setIsOpen(true), setId(movie.id)]}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                ></img>
                 <h1>{movie.original_title}</h1>
               </div>
             ))}
-          <img
-            src={url}
-            alt="figth club"
-            onClick={() => [setShow(true), console.log(show)]}
-          />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
+          {isOpen && <ModalMovie setIsOpen={setIsOpen} id={id} />}
         </section>
         {/* Top rated: */}
-        <h3>Top rated:</h3>
-        <section>
-          {/* map out posters onClick=modal*/}
-          <img
-            src={url}
-            alt="figth club"
-            onClick={() => [setShow(true), console.log(show)]}
-          />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-          <img src={url} alt="figth club" />
-        </section>
       </main>
     </div>
   );
