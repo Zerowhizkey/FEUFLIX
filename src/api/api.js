7b019678d6bd59e2bc09fe24fbc2dc76f098dd69
Zerowhizkey <zerogranit@hotmail.se>
2022-11-02 10:06:51 +0100

    updated api

--- src/api/api.js
+++ src/api/api.js
@@ -9,3 +9,15 @@ export const getMovies = async () => {
 	console.log(data);
 	return data;
 };
+
+export const searchMovies = async (query) => {
+	const { data } = await axios.get(
+		`${import.meta.env.VITE_API_SEARCH}search/movie?api_key=${
+			import.meta.env.VITE_API_KEY
+		}&query=${query}`
+	);
+	console.log(data);
+	return data;
+};
+
+// const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=<<api_key_here>>&query";
